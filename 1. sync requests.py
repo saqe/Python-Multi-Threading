038c8bba1942d0aca89dd9e2db156e03df529b3a
Saqib <18554574+saqe@users.noreply.github.com>
2021-09-20 16:02:05 +0500

    👌 IMPROVE: perf_counter with time.

--- 1. sync requests.py
+++ 1. sync requests.py
@@ -1,15 +1,15 @@
 import requests
-import time
+from time import perf_counter
 
-start = time.time()
+start = perf_counter()
 
 def downloader(id):
-    start = time.time()
+    start = perf_counter()
     print(f"Task# {id}")
     requests.get("https://httpbin.org/get").json()
-    print(f"Task# {id} ended. Time taken {time.time() - start}")
+    print(f"Task# {id} ended. Time taken {(perf_counter() - start):.2f}s")
 
 for i in range(0, 50):
     downloader(i)
 
-print(f"Total time taken {time.time() - start}")
+print(f"Total time taken {(perf_counter() - start):.2f}s")
--- 2. multitasking.py
+++ 2. multitasking.py
@@ -1,20 +1,20 @@
 import multitasking
 import requests
-import time
+from time import perf_counter
 
 multitasking.set_max_threads(10)
 
-start = time.time()
+start = perf_counter()
 
 @multitasking.task
 def downloader(id):
-    start = time.time()
+    start = perf_counter()
     print(f"Task# {id}")
     requests.get("https://httpbin.org/get").json()
-    print(f"Task# {id} ended. Time taken {time.time() - start}")
+    print(f"Task# {id} ended. Time taken {(perf_counter() - start):.2f}s")
 
 for i in range(0, 50):
     downloader(i)
 
 multitasking.wait_for_tasks()
-print(f"Total time taken: {time.time() - start} seconds")
+print(f"Total time taken {(perf_counter() - start):.2f}s")
